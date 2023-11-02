e25b209d18bea56f40367d9eb93a5411000f7e35
Johan Schonning <jschonning@salesforce.com>
2023-11-02 12:53:06 -0700

    feat(salesforcecli): updating snapshot

--- src/utils/prototypes.ts
+++ src/utils/prototypes.ts
@@ -1,19 +1,18 @@
 /* eslint-disable */
-String.prototype.formatUnicorn = String.prototype.formatUnicorn ||
-function () {
-    "use strict";
+String.prototype.formatUnicorn =
+  String.prototype.formatUnicorn ||
+  function () {
+    'use strict';
     var str = this.toString();
     if (arguments.length) {
-        var t = typeof arguments[0];
-        var key;
-        var args = ("string" === t || "number" === t) ?
-            Array.prototype.slice.call(arguments)
-            : arguments[0];
+      var t = typeof arguments[0];
+      var key;
+      var args = 'string' === t || 'number' === t ? Array.prototype.slice.call(arguments) : arguments[0];
 
-        for (key in args) {
-            str = str.replace(new RegExp("\\{" + key + "\\}", "gi"), args[key]);
-        }
+      for (key in args) {
+        str = str.replace(new RegExp('\\{' + key + '\\}', 'gi'), args[key]);
+      }
     }
 
     return str;
-};
+  };

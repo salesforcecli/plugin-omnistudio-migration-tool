efc1cc056286e0c78f51b177ed2b4c24a831089e
abhinavkumar2 <abhinavkumar2@salesforce.com>
2024-08-20 13:54:01 +0530

    fix: fixing the lint issues
    Signed-off-by: abhinavkumar2 <abhinavkumar2@salesforce.com>

--- src/utils/apex/parser/apexparser.ts
+++ src/utils/apex/parser/apexparser.ts
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import {
   ApexLexer,
   CommonTokenStream,
@@ -6,62 +7,55 @@ import {
   ApexParserListener,
   ClassDeclarationContext,
   DotExpressionContext,
-  VariableDeclaratorContext
-} from "@apexdevtools/apex-parser";
-import {
-  CharStreams,
-} from "antlr4ts";
+  VariableDeclaratorContext,
+  CompilationUnitContext,
+} from '@apexdevtools/apex-parser';
+import { CharStreams } from 'antlr4ts';
 import { ParseTreeWalker } from 'antlr4ts/tree/ParseTreeWalker';
-import fs from 'fs';
 
 export class ApexASTTraverser {
-
-  public static parse(apexClass: string): any {
-    let lexer = new ApexLexer(new CaseInsensitiveInputStream(CharStreams.fromString(apexClass)));
-    let tokens = new CommonTokenStream(lexer);
-    let parser = new ApexParser(tokens);
-    let context = parser.compilationUnit();
+  public static parse(apexClass: string): CompilationUnitContext {
+    const lexer = new ApexLexer(new CaseInsensitiveInputStream(CharStreams.fromString(apexClass)));
+    const tokens = new CommonTokenStream(lexer);
+    const parser = new ApexParser(tokens);
+    const context = parser.compilationUnit();
     //  parser.addParseListener(new interfaceVisitor() as ApexParserListener);
-    ParseTreeWalker.DEFAULT.walk(
-      new interfaceVisitor() as ApexParserListener,
-      context
-    );
+    ParseTreeWalker.DEFAULT.walk(new interfaceVisitor() as ApexParserListener, context);
     return context;
   }
 
-  public static traverse(filePath: string): any {
-    let fileContent = fs.readFileSync(filePath).toString();
-    let ast = this.parse(fileContent);
+  public static traverse(filePath: string): CompilationUnitContext {
+    const fileContent = fs.readFileSync(filePath).toString();
+    const ast = this.parse(fileContent);
     return ast;
   }
 }
 export class interfaceVisitor implements ApexParserListener {
-  enterClassDeclaration(ctx: ClassDeclarationContext): void {
-    console.log(ctx.typeList().typeRef(0).typeName(0).id().Identifier().symbol.text);
-  };
-  enterDotExpression(ctx: DotExpressionContext): void {
-    console.log("*********")
-    console.log(ctx.expression().start.text);
+  private interfaceImplementations: string[] = [];
+
+  public enterClassDeclaration(ctx: ClassDeclarationContext): void {
+    this.interfaceImplementations.push(ctx.typeList().typeRef(0).typeName(0).id().Identifier().symbol.text);
+  }
+  public enterDotExpression(ctx: DotExpressionContext): void {
+    // console.log('*********');
+    // console.log(ctx.expression().start.text);
 
     if (ctx.dotMethodCall()) {
-      console.log(ctx.dotMethodCall().anyId().Identifier().symbol.text);
-      //ctx.dotMethodCall().expressionList().expression(1).children[0].children[0].children[0] 
-      console.log(ctx.dotMethodCall().expressionList().expression(1).children[0]);
+      // console.log(ctx.dotMethodCall().anyId().Identifier().symbol.text);
+      // ctx.dotMethodCall().expressionList().expression(1).children[0].children[0].children[0]; 
+      // console.log(ctx.dotMethodCall().expressionList().expression(1).children[0]);
 
     }
-    console.log("*********")
+    // console.log('*********');
   }
-  enterVariableDeclarator(ctx: VariableDeclaratorContext): void {
+  public enterVariableDeclarator(ctx: VariableDeclaratorContext): void {
     if (ctx.id().Identifier().symbol.text === 'DRName') {
-      console.log(ctx.expression());
+      // console.log(ctx.expression());
     }
   }
-
 }
 
+const filePath = '/Users/abhinavkumar2/company/plugin-omnistudio-migration-tool/test/FormulaParserService.cls';
+ApexASTTraverser.traverse(filePath);
 
-
-let filePath = '/Users/abhinavkumar2/company/plugin-omnistudio-migration-tool/test/FormulaParserService.cls';
-let ast = ApexASTTraverser.traverse(filePath);
-
-console.log(ast);
+// console.log(ast);

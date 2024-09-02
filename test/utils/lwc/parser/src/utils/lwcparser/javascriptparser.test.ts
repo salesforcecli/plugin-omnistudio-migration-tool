53f27188d7ada1b3899204809be84aed0fa09cdb
Shailesh Pachbhai <spachbhai@salesforce.com>
2024-09-02 23:21:06 +0530

    feat: add unit tests for lwc parser

---
+++ test/utils/lwc/parser/src/utils/lwcparser/javascriptparser.test.ts
@@ -0,0 +1,28 @@
+import JavaScriptParser from '../../../../../../../src/utils/lwcparser/jsparser/JavaScriptParser';
+// eslint-disable-next-line import/order
+import { expect } from 'chai';
+
+describe('JavaScriptParser file', () => {
+  const mockFilePath = 'src/utils/lwcparser/input/test.js';
+
+  it('should parse file content to AST correctly', () => {
+    const jsParser: JavaScriptParser = new JavaScriptParser(mockFilePath);
+    jsParser.parseFile();
+    // eslint-disable-next-line no-unused-expressions
+    expect(jsParser['ast']).not.null;
+    expect(jsParser['ast']?.type).to.be.equal('File'); // Check if the AST root node is of type 'File'
+  });
+
+  it('should traverse AST and find import declarations', () => {
+    const jsParser = new JavaScriptParser(mockFilePath);
+    jsParser.parseFile();
+    expect(jsParser.traverseAST()[0]).to.have.to.be.contains('lwc');
+  });
+
+  it('should throw an error if traverseAST is called before parseFile', () => {
+    const jsParser = new JavaScriptParser(mockFilePath);
+    expect(() => {
+      jsParser.traverseAST();
+    }).to.Throw('AST has not been generated. Call parseFile() first.');
+  });
+});

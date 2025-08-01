a5a36c0a3610b495cdea0dad32b8cee23087ff92
Rajender Kumar <rajender.kumar@salesforce.com>
2025-08-01 12:19:09 +0530

    fix: urls for ip

--- src/utils/constants/documentRegistry.ts
+++ src/utils/constants/documentRegistry.ts
@@ -52,9 +52,9 @@ export const documentRegistry = {
   dataRaptorNameChangeMessage:
     'https://help.salesforce.com/s/articleView?id=xcloud.os_migrate_update_references_to_dataraptors_from_apex_after_migration.htm&type=5',
   integrationProcedureNameChangeMessage:
-    'https://help.salesforce.com/s/articleView?id=xcloud.os_migrate_update_references_to_integration_procedures_after_migration.htm&type=5',
+    'https://help.salesforce.com/s/articleView?id=xcloud.os_migrate_update_references_to_dataraptors_from_apex_after_migration.htm&type=5',
   integrationProcedureManualUpdateMessage:
-    'https://help.salesforce.com/s/articleView?id=xcloud.os_migrate_update_references_to_integration_procedures_after_migration.htm&type=5',
+    'https://help.salesforce.com/s/articleView?id=xcloud.os_migrate_update_references_to_dataraptors_from_apex_after_migration.htm&type=5',
   duplicateCardNameMessage: 'https://help.salesforce.com/s/articleView?id=xcloud.os_clone_a_flexcard.htm&type=5',
   duplicateGlobalAutoNumberNameMessage:
     'https://help.salesforce.com/s/articleView?id=xcloud.os_omnistudio_naming_conventions.htm&type=5',
--- test/utils/constants/documentRegistry.test.ts
+++ test/utils/constants/documentRegistry.test.ts
@@ -36,8 +36,8 @@ const titles = {
   omniScriptNameChangeMessage:
     'Update Omniscript Custom Lightning Web Components and Omniscript Elements Overridden with Customized Components',
   dataRaptorNameChangeMessage: 'Update References to Omnistudio Components After Migration',
-  integrationProcedureNameChangeMessage: 'Update Integration Procedure Type and Subtype After Migration',
-  integrationProcedureManualUpdateMessage: 'Update Integration Procedure Type and Subtype After Migration',
+  integrationProcedureNameChangeMessage: 'Update References to Omnistudio Components After Migration',
+  integrationProcedureManualUpdateMessage: 'Update References to Omnistudio Components After Migration',
   duplicateCardNameMessage: 'Clone a Flexcard',
 };
 

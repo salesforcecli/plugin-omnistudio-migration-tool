7bc540aa9783044df1e106a093ddbceaecc68b6d
sf-vijaychetan-pabbisetty <64299293+sf-vijaychetan-pabbisetty@users.noreply.github.com>
2026-06-23 15:08:14 -0700

    Update dataraptor.ts

--- src/migration/dataraptor.ts
+++ src/migration/dataraptor.ts
@@ -550,7 +550,7 @@ export class DataRaptorMigrationTool extends BaseMigrationTool implements Migrat
 
     mappedObject['Name'] = this.cleanName(mappedObject['Name']);
     mappedObject['IsActive'] = true;
-    mappedObject['IsEmptyInputAllowed'] = true;
+    mappedObject['IsMigrated'] = true; 
 
     // BATCH framework requires that each record has an "attributes" property
     mappedObject['attributes'] = {
